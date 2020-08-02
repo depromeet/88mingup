f947e696fb1d845e285e6e1c27fec0bb879628c1
Ssook <cesces333@naver.com>
2020-08-02 16:26:47 +0900

    [back] article app 이미지 추가 (#3)
    * wiP
    
    * 파일 정리
    
    * [back] review 반영
    
    * [back] Images->MediaContent
    
    * [back] Images->MediaContent(2)
    
    * [back] backenv 삭제
    
    * [back] DB postgre로 바꿈
    
    Co-authored-by: sichoi <choiseungil29@gmail.com>

--- back/article/serializers.py
+++ back/article/serializers.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, MediaContent
 from rest_framework.serializers import ModelSerializer
 
+class ImageSerializer(ModelSerializer):
+    class Meta:
+        model=MediaContent
+        fields=[
+            "article",
+            "file",
+        ]
 class ArticleSerializer(ModelSerializer):
+    images_set=ImageSerializer(many=True,read_only=True)
+
     class Meta:
         model = Article
         fields=[
-            'title',
-            'content',
-            'lat',
-            'lng',
-            'writer'
+            "title",
+            "content",
+            "lat",
+            "lng",
+            "writer",
+            "images_set",
         ]
--- back/warmingup/urls.py
+++ back/warmingup/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from health.views import HealthCheckView
@@ -21,4 +23,4 @@ urlpatterns = [
     path("health/", HealthCheckView.as_view()),
     path("admin/", admin.site.urls),
     path("api/v1/", include("article.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
