e1c196238c77906188aee6e0207895b920e0a661
ssook <cesces333@naver.com>
2020-08-08 20:28:04 +0900

    [back] 생성날짜, 인기순으로 정렬해서 데이터 제공

--- back/article/views.py
+++ back/article/views.py
@@ -3,8 +3,6 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Article, MediaContent
 from .serializers import ArticleSerializer, MediaContentSerializer
-from haversine import haversine
-from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import ArticleFilter
 from rest_framework.filters import OrderingFilter
@@ -14,29 +12,8 @@ class ArticleViewSet(ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     filter_backends = (DjangoFilterBackend,OrderingFilter,)
-    filterset_class=ArticleFilter
-    ordering_fields=['lat','popularity','lng']
-
-    # def get_queryset(self):
-    #     print("test",self.request.query_params.get("user_lat",""))
-    #     print("ttttest", self.request.query_params.get("user_lng",""))
-    #     return self.queryset\
-    #         .filter(lat=self.request.query_params.get('user_lat'))\
-    #         .filter(lng=self.request.query_params.get('user_lng'))
-        # qs=super().get_queryset()
-        # user_lat = self.request.query_params.get("user_lat","")
-        # user_lng = self.request.query_params.get("user_lng", "")
-        # near_article=[]
-        #
-        # if(user_lat and user_lng):
-        #     user_position = (37.5, 126.6)
-        #     for i in range(len(qs)) :
-        #         article_position=(qs[i].lat,qs[i].lng)
-        #         if(haversine(user_position,article_position) < 20) :
-        #             near_article.append(qs[i])
-        #
-        # return near_article
-
+    filterset_class = ArticleFilter
+    ordering_fields = ["lat","popularity","lng","created_at","updated_at"]
 
 class MediaContentViewSet(ModelViewSet):
     queryset = MediaContent.objects.all()
--- back/warmingup/settings/base.py
+++ back/warmingup/settings/base.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
     ),
 }
 
