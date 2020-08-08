4e150a3dd76148764de3433ed84e318782a2ff6a
ssook <cesces333@naver.com>
2020-08-08 19:41:31 +0900

    [back] 위도 경도 필터링 단일값 됨

---
+++ back/article/filters.py
@@ -0,0 +1,15 @@
+# from .models import Article
+# from django_filters import rest_framework as filters
+#
+# class ArticleFilter(filters.FilterSet):
+#     # min_lat = filters.NumberFilter(field_name="lat", lookup_expr='gte')
+#     # min_lng = filters.NumberFilter(field_name="lng", lookup_expr='gte')
+#     # max_lat = filters.NumberFilter(field_name="lat", lookup_expr='lte')
+#     # max_lng = filters.NumberFilter(field_name="lng", lookup_expr='lte')
+#     class Meta:
+#         model = Article
+#         fields = [
+#             "lat",
+#             "lng"
+#         ]
+#
--- back/article/views.py
+++ back/article/views.py
@@ -4,24 +4,37 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Article, MediaContent
 from .serializers import ArticleSerializer, MediaContentSerializer
 from haversine import haversine
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
+# from .filters import ArticleFilter
+
 
 class ArticleViewSet(ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields=('lat','lng')
 
-    def get_queryset(self):
-        qs=super().get_queryset()
-        user_lat = self.request.query_params.get("user_lat","")
-        user_lng = self.request.query_params.get("user_lng", "")
-        user_position = (37.5, 126.6)  # (lat, lon)
-        return_qs=[]
 
-        for i in range(len(qs)) :
-            article_position=(qs[i].lat,qs[i].lng)
-            if(haversine(user_position,article_position) < 20) :
-                return_qs.append(qs[i])
-
-        return return_qs
+    # def get_queryset(self):
+    #     print("test",self.request.query_params.get("user_lat",""))
+    #     print("ttttest", self.request.query_params.get("user_lng",""))
+    #     return self.queryset\
+    #         .filter(lat=self.request.query_params.get('user_lat'))\
+    #         .filter(lng=self.request.query_params.get('user_lng'))
+        # qs=super().get_queryset()
+        # user_lat = self.request.query_params.get("user_lat","")
+        # user_lng = self.request.query_params.get("user_lng", "")
+        # near_article=[]
+        #
+        # if(user_lat and user_lng):
+        #     user_position = (37.5, 126.6)
+        #     for i in range(len(qs)) :
+        #         article_position=(qs[i].lat,qs[i].lng)
+        #         if(haversine(user_position,article_position) < 20) :
+        #             near_article.append(qs[i])
+        #
+        # return near_article
 
 
 class MediaContentViewSet(ModelViewSet):
