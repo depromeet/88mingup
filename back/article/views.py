cf4f16d2b37f514ac02226394da55eca84cbbb7a
ssook <cesces333@naver.com>
2020-08-08 20:39:15 +0900

    min, max 위도 경도 값으로 article filtering

--- back/article/views.py
+++ back/article/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Article, MediaContent
 from .serializers import ArticleSerializer, MediaContentSerializer
@@ -13,7 +11,7 @@ class ArticleViewSet(ModelViewSet):
     serializer_class = ArticleSerializer
     filter_backends = (DjangoFilterBackend,OrderingFilter,)
     filterset_class = ArticleFilter
-    ordering_fields = ["lat","popularity","lng","created_at","updated_at"]
+    ordering_fields = ["lat","lng","popularity","created_at","updated_at"]
 
 class MediaContentViewSet(ModelViewSet):
     queryset = MediaContent.objects.all()
