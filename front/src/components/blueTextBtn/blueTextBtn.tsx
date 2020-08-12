1b5de5c5fcbfb246d75eef078324e3fa3987f30e
jeongmin <danmin20@khu.ac.kr>
2020-08-12 22:42:26 +0900

    children을 렌더링하도록 수정

--- front/src/components/blueTextBtn/blueTextBtn.tsx
+++ front/src/components/blueTextBtn/blueTextBtn.tsx
@@ -4,19 +4,18 @@ interface btnProps {
   style?: React.CSSProperties;
   className?: string;
   isActive?: Boolean;
-  text?: string | undefined;
   onClick?: () => void;
 }
 
 const blueTextBtn: React.FC<btnProps> = (props) => {
-  const { style, className, isActive, text, onClick } = props;
+  const { style, className, isActive, children, onClick } = props;
   return (
     <div
       className={className}
       style={{ color: isActive ? '#373cff' : '#a1a1a1' }}
       onClick={onClick}
     >
-      {text}
+      {children}
     </div>
   );
 };
