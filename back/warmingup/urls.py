f229f36681f7d8765f351866184174fc4998eeaf
sichoi <choiseungil29@gmail.com>
2020-08-15 22:51:48 +0900

    [back] redoc 제거

--- back/warmingup/urls.py
+++ back/warmingup/urls.py
@@ -47,13 +47,8 @@ if settings.DEBUG:
             name="schema-json",
         ),
         re_path(
-            r"^swagger/$",
+            "swagger",
             schema_view.with_ui("swagger", cache_timeout=0),
             name="schema-swagger-ui",
-        ),
-        re_path(
-            r"^redoc/$",
-            schema_view.with_ui("redoc", cache_timeout=0),
-            name="schema-redoc",
         ),
     ]
