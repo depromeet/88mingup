3b2859a79f4422c9d032578f314a01ffba7c38d5
Ssook <cesces333@naver.com>
2020-08-17 00:53:40 +0900

    좋아요 기능 관련 모델, api구현 (#26)
    좋아요 기능 구현

--- back/user/views.py
+++ back/user/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import authenticate, login
+from django.views.generic.base import TemplateResponseMixin
+from rest_framework import generics, response
+from rest_framework.exceptions import ValidationError
+from rest_framework.renderers import TemplateHTMLRenderer
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from rest_framework import response
