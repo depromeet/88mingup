acf1b3fd837795ee63fb77c6d85ca9e9becb13a9
sichoi <choiseungil29@gmail.com>
2020-08-17 00:10:21 +0900

    [front/main] 스타일 수정

--- front/src/components/article/shortcut.tsx
+++ front/src/components/article/shortcut.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 import { ArticleEntityStateProps } from 'store/article/reducer';
+import styled from '@emotion/styled';
 
 interface Props extends ArticleEntityStateProps {}
 
@@ -27,12 +28,35 @@ export const ArticleShortcut = (props: Props) => {
             justify-content: space-between;
           `}
         >
-          <span>{props.title}</span>
-          <div>좋아요 및 코멘트</div>
+          <Title>{props.title}</Title>
+          <div>
+            <SubTitle>1023</SubTitle>
+            <SubTitle>1023</SubTitle>
+          </div>
         </div>
-        <span>N시간 전</span>
-        <p>{props.description}</p>
+        <TimeAgo>3시간 전</TimeAgo>
+        <Description>{props.description}</Description>
       </div>
     </div>
   );
 };
+
+const Title = styled.span`
+  font-size: 18px;
+  color: black;
+`;
+
+const SubTitle = styled.span`
+  font-size: 11px;
+  color: black;
+`;
+
+const TimeAgo = styled.span`
+  font-size: 9px;
+  color: #a1a1a1;
+`;
+
+const Description = styled.p`
+  font-size: 13px;
+  color: black;
+`;
