80a53b6d3a271efc73349f312616265f0fded229
jeongmin <danmin20@khu.ac.kr>
2020-08-18 00:44:51 +0900

    위치검색 페이지 구현

---
+++ front/src/components/searchInput/searchInput.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './searchInput.scss';
+import { SearchIcon } from 'assets';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  style?: React.CSSProperties;
+  placeholder?: string;
+  required?: boolean;
+  value: string;
+  type?: string;
+  onChange: React.EventHandler<any>;
+}
+
+const container: React.CSSProperties = {
+  fontSize: '19px',
+  padding: '14px 0px',
+  border: '0px solid white',
+  borderBottomWidth: '2px',
+  fontWeight: 'bold',
+  margin: '62px 16px',
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+const input: React.CSSProperties = {
+  border: '0px',
+  backgroundColor: '#373cff',
+  width: '100%',
+  color: 'white',
+};
+
+const UnderlineInput: React.FC<Props> = (props) => {
+  const {
+    className,
+    style,
+    placeholder,
+    required,
+    value,
+    onChange,
+    type,
+  } = props;
+  return (
+    <div style={{ ...container, ...style }}>
+      <input
+        style={{ ...input, ...style }}
+        className={className}
+        placeholder={placeholder}
+        required={required}
+        value={value}
+        onChange={onChange}
+        type={type}
+      />
+      <SearchIcon />
+    </div>
+  );
+};
+
+export default UnderlineInput;
--- front/src/pages/search-location/index.tsx
+++ front/src/pages/search-location/index.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Header, Input, UnderlineInput } from 'components';
+import { HeaderItem } from 'components/header/item';
+import { history } from 'store/rootReducer';
+import { CloseIcon } from 'assets';
+import { useInput } from 'hooks/useInput';
+// import Geocode from 'react-geocode';
 
 interface Props {
   style?: React.CSSProperties;
   className?: string;
 }
 
+const container: React.CSSProperties = {
+  backgroundColor: '#373cff',
+  height: '100vh',
+};
+
 const SearchLocationPage: React.FC<Props> = (props) => {
   const { className, style } = props;
+  const search = useInput('');
 
-  return <div></div>;
+  return (
+    <div className={className} style={{ ...style, ...container }}>
+      <Header>
+        <HeaderItem
+          align="start"
+          onClick={() =>
+            history.length > 0 ? history.goBack() : history.push('/')
+          }
+        >
+          <CloseIcon />
+        </HeaderItem>
+        <HeaderItem align="middle">
+          <span style={{ fontSize: '15px', color: 'white' }}>위치 검색</span>
+        </HeaderItem>
+        <HeaderItem align="end" />
+      </Header>
+      <div>
+        <UnderlineInput
+          value={search.value}
+          onChange={search.onChange}
+          placeholder="위치를 입력해주세요"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchLocationPage;
