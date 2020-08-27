0dcb0b1ce1a17ddabc178cd1366db5f65937bc4a
Ubuntu <ubuntu@ip-172-31-24-226.ap-northeast-2.compute.internal>
2020-08-27 16:52:25 +0000

    ㅈㅗㅎ아요 반영

--- back/article/views.py
+++ back/article/views.py
@@ -1,6 +1,8 @@
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
 from django.http import HttpResponse
+from django.db.models import F, Sum, Count, Case, When
+
 from django_filters.rest_framework import DjangoFilterBackend
 from requests import Response
 from rest_framework import status, viewsets
@@ -33,13 +35,15 @@ class ArticleViewSet(viewsets.ModelViewSet):
         OrderingFilter,
     )
     filterset_class = ArticleFilter
-    ordering_fields = ["created_at", "distance"]
+    ordering_fields = ["created_at", "distance", 'like_count']
 
     def get_queryset(self):
         request = self.request
         params = request.query_params
         lat = params.get("lat")
         lng = params.get("lng")
+        
+        self.queryset = self.queryset.annotate(like_count=Count('like_users'))
 
         if not lat or not lng:
             return self.queryset
