a61c5b1891d939c73fb86a7d5d3fa6727e1da3ac
youngje jo <youngje4284@woowahan.com>
2020-08-29 00:25:33 +0900

    카드 컴포넌트 동영상 가능하게 구현

--- front/src/components/card/card.tsx
+++ front/src/components/card/card.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 
 interface CardProps {
   style?: React.CSSProperties;
-  url?: string | URL;
+  url: string;
   className?: string;
 }
 
@@ -17,7 +17,27 @@ const CardDiv = styled.div`
 
 const Card: React.FC<CardProps> = (props) => {
   const { children, style, url, className } = props;
-  return (
+
+  const splitUrl = url.split('.');
+
+  const extension = splitUrl[splitUrl.length - 1];
+
+  const isVideo =
+    extension === 'mov' || extension === 'mp4' || extension === 'avi';
+
+  return isVideo ? (
+    <video
+      style={{
+        borderRadius: '16px',
+        width: '100%',
+        height: '168px',
+        background: '#f2f2f2',
+        backgroundSize: 'cover',
+      }}
+      autoPlay
+      src={url}
+    />
+  ) : (
     <CardDiv
       className={className}
       style={{ ...style, backgroundImage: `url(${url})` }}
